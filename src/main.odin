a94ede595b70f7249d93030825d8b80bd41ef8a7
CHANGEME <CHANGEME>
2023-12-15 22:47:05 -0300

    finish the integration with LUA OTP 2FA library

--- src/main.odin
+++ src/main.odin
@@ -1,7 +1,7 @@
 package twofa
 
-import "core:fmt"
 import libc "core:c"
+import "core:fmt"
 import lua51 "vendor:lua/5.1"
 
 
@@ -12,7 +12,7 @@ main :: proc() {
 
   lua51.L_openlibs(StateL)
 
-  ret: lua51.Status = lua51.L_loadfile(StateL, "./src/test.lua")
+  ret: lua51.Status = lua51.L_loadfile(StateL, "./src/otp_lib.lua")
 
   if !(ret == lua51.Status.OK) {
     lua51.L_error(StateL, "lua LoadFile Failed\n")
@@ -29,7 +29,27 @@ main :: proc() {
 
   fmt.println(StateL)
 
+  string_fun_call: cstring = "twofa_hash_gen"
+  lua51.getglobal(StateL, string_fun_call)
+
+  string_test: cstring = "2FASTEST"
+  lua51.pushstring(StateL, string_test)
+
+
+  int_ret = lua51.pcall(StateL, 1, 1, 0)
+  if !(int_ret == 0) {
+    fmt.println(StateL)
+    lua51.L_error(StateL, "lua pcall() Failed\n")
+  }
+
+
+  ret_fun_lua_call: cstring = lua51.tostring(StateL, 1)
+
+  fmt.println("LUA RETURN :: ", ret_fun_lua_call)
+
+
   fmt.println("Back in Odin again\n")
+  lua51.close(StateL)
 
   return
 
--- src/otp_lib.lua
+++ src/otp_lib.lua
@@ -1,16 +1,23 @@
-package.path = package.path .. ';' .. '../lib/LuaOTP/src/?.lua'
-package.path = package.path .. ';' .. '../lib/LuaOTP/2FA/?.lua'
+package.path = package.path .. ';' .. './lib/LuaOTP/src/?.lua'
+package.path = package.path .. ';' .. './lib/LuaOTP/2FA/?.lua'
+package.path = package.path .. ';' .. './lib/inspect2/?.lua'
+
+-- print(package.path)
 
 local INIP = require ('ini')
 local OTP = require ('otp')
 local TOTP = require ('totp')
+local inspect = require('inspect2')
+
 
 local INTERVAL      = 30;
 local DIGITS        = 6;
 local DIGEST        = "SHA1";
 
 -- Data loading
-local data = INIP.load('data_digest.ini');
+--[[
+
+local data = INIP.load('src/data_digest.ini');
 
 for k, v in pairs( data ) do
 
@@ -27,4 +34,25 @@ for k, v in pairs( data ) do
   print("TOTP Generated: `" .. totp_data_or_err_1 .. "`")
 
   print("")
+end
+--]]--
+
+function twofa_hash_gen(digester_secret)
+
+  local BASE32_SECRET = digester_secret
+
+  -- Create OTPData struct, which decides the environment
+  OTP.type = "totp"
+  local tdata = OTP.new(BASE32_SECRET, DIGITS, DIGEST, 30)
+
+  -- totp.now
+  local totp_data_or_err_1 = TOTP.now(tdata)
+
+  return totp_data_or_err_1
+end
+
+
+function read_ini (file_name)
+  local data = INIP.load('src/data_digest.ini');
+  return data
 end
